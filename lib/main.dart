953eb291315434ddb1507b36486799c2683b8920
Ramses Ryan Dineros <ramsesryandineros@gmail.com>
2021-05-16 14:20:05 +0800

    add firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,45 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+
 import 'package:iq_trace/screens/home/home_screen.dart';
 import 'package:iq_trace/constants.dart';
 import './screens/update/symptom_update_screen.dart';
 import './screens/scanner/qr_scanner_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('Oops, something went wrong!'),
+          );
+        } else if (snapshot.connectionState == ConnectionState.done) {
+          return IQTrace();
+        } else {
+          return CircularProgressIndicator();
+        }
+      },
+    );
+  }
+}
+
+class IQTrace extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
