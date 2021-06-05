96da5b65c00f7465a98489a2b4d49e05a98750b3
Ramses Ryan Dineros <ramsesryandineros@gmail.com>
2021-06-05 22:35:40 +0800

    change launcher icon, ready app for release

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 const app_title = 'IQTrace';
-final iqtPrimaryColor = Color(0xFF018348); //#F51720
+final iqtPrimaryColor = Color(0xFF018348); //#018348
 final iqtSecondaryColor = Color(0xFFFDB926); //#fdb926
