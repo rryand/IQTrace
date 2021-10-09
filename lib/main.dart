cef4cae04f6f85a71a6e86231eaff1d1c6b7eb9d
ramsesryandineros <ramsesryandineros@gmail.com>
2021-10-09 16:02:21 +0800

    refactor: initial error screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:camera/camera.dart';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:iq_trace/networking/api_response.dart';
+import 'package:iq_trace/screens/admin/admin_screen.dart';
+import 'package:iq_trace/screens/error/error_screen.dart';
 import 'package:iq_trace/screens/home/home_screen.dart';
 import 'package:iq_trace/constants.dart';
 import 'package:iq_trace/screens/update/exposure_update_screen.dart';
@@ -36,7 +38,6 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   final _auth = AuthService();
-  final _isError = false;
 
   @override
   Widget build(BuildContext context) {
@@ -46,26 +47,20 @@ class _AppState extends State<App> {
         if (snapshot.hasData && snapshot.connectionState == ConnectionState.done) {
           switch (snapshot.data!.status) {
             case Status.COMPLETED:
-              return IQTrace(snapshot.data!.data);
+              return IQTrace(user: snapshot.data!.data);
             case Status.ERROR:
-              return Directionality(
-                textDirection: TextDirection.ltr,
-                child: Center(
-                  child: Text(snapshot.data!.message!),
-                ),
+              return IQTrace(
+                errorMessage: snapshot.data!.message,
+                onError: () => setState(() {}),
               ); // TODO: make better implementation, change to stream?
-              /* return ErrorScreen(
-                errorMessage: snapshot.data!.message!,
-                onRetryPressed: () => 
-                  Navigator
-                    .of(context)
-                    .pop(),
-              ); */
             case Status.LOADING:
               break;
           }
         } else {
-          return CircularProgressIndicator();
+          return Container(
+            color: Colors.white,
+            child: Center(child: CircularProgressIndicator()),
+          );
         }
         throw 'Unhandled null return';
       },
@@ -74,9 +69,11 @@ class _AppState extends State<App> {
 }
 
 class IQTrace extends StatelessWidget {
-  IQTrace(this.user);
+  IQTrace({ this.user, this.errorMessage, this.onError });
 
   final User? user;
+  final String? errorMessage;
+  final Function? onError;
 
   @override
   Widget build(BuildContext context) {
@@ -91,27 +88,54 @@ class IQTrace extends StatelessWidget {
           FocusManager.instance.primaryFocus!.unfocus();
         }
       },
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primaryColor: iqtPrimaryColor,
-          secondaryHeaderColor: iqtSecondaryColor,
-          appBarTheme: AppBarTheme(
-            backgroundColor: iqtPrimaryColor,
-          ),
+      child: errorMessage == null ? _buildApp(context, initialRoute) 
+        : _buildErrorPage(context, errorMessage!),
+    );
+  }
+
+  Widget _buildApp(BuildContext context, String initialRoute) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: iqtPrimaryColor,
+        secondaryHeaderColor: iqtSecondaryColor,
+        appBarTheme: AppBarTheme(
+          backgroundColor: iqtPrimaryColor,
         ),
-        initialRoute: initialRoute,
-        routes: {
-          '/home': (context) => HomeScreen(title: app_title),
-          '/login': (context) => LoginScreen(),
-          '/register': (context) => RegisterScreen(cameras),
-          '/register/camera': (context) => CameraScreen(cameras),
-          '/register/camera/image': (context) => DisplayImageScreen(),
-          '/update': (context) => SymptomUpdateScreen(),
-          '/update/exposure': (context) => ExposureUpdateScreen(),
-          '/update/quarantine': (context) => QuarantineUpdateScreen(),
-          '/scanner': (context) => QRScannerScreen(),
-        },
+      ),
+      initialRoute: initialRoute,
+      routes: {
+        '/home': (context) => HomeScreen(title: app_title),
+        '/login': (context) => LoginScreen(),
+        '/register': (context) => RegisterScreen(cameras),
+        '/register/camera': (context) => CameraScreen(cameras),
+        '/register/camera/image': (context) => DisplayImageScreen(),
+        '/update': (context) => SymptomUpdateScreen(),
+        '/update/exposure': (context) => ExposureUpdateScreen(),
+        '/update/quarantine': (context) => QuarantineUpdateScreen(),
+        '/scanner': (context) => QRScannerScreen(),
+        '/admin': (context) => AdminScreen(),
+      },
+    );
+  }
+
+  Widget _buildErrorPage(BuildContext context, String errorMessage) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: iqtPrimaryColor,
+        secondaryHeaderColor: iqtSecondaryColor,
+        appBarTheme: AppBarTheme(
+          backgroundColor: iqtPrimaryColor,
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('IQTrace'),
+        ),
+        body: ErrorScreen(
+          errorMessage: errorMessage,
+          onRetryPressed: () => onError!(),
+        ),
       ),
     );
   }
